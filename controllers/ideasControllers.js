4d350f464757660ec900b3c4c5bc7e8e9abfbba3
VitaliyYakovenko <yakovenkovitaly1@gmail.com>
2023-06-12 10:57:44 +0300

    add pagination

--- controllers/ideasControllers.js
+++ controllers/ideasControllers.js
@@ -2,14 +2,23 @@ const Idea = require("../model/idea");
 const HttpError = require("../halpers/HttpError");
 const { ideaStatusSchema ,ideaDoneSchema} = require("../utils/index");
 
-const listIdeas = async ( _ , res, next) => {
+
+
+const listIdeas = async (req, res, next) => {
+    console.log(req.query);
+  
     try {
-        const result = await Idea.find();
-        if (!result) {
+        const { page = 1, limit = 5 } = req.query;
+        const skip = (page - 1) * limit; 
+        
+        const data = await Idea.find({}, "", { skip, limit });
+        const { length: total } = await Idea.find(); 
+
+        if (!data) {
             throw HttpError(500, "Server error");
         };
 
-        res.json(result);
+        res.json({data, total});
     } catch (error) {
         next(error);
     }
